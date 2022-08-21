46eaf16813a473bee22b567bc08271cb2baff03d
Evgeniya <evgeniya.gorina@bcvsolutions.eu>
2022-08-21 20:26:09 +0200

    Coffe selector with popup and size buttons was added

--- kod/script1.js
+++ kod/script1.js
@@ -20,3 +20,50 @@ for (let i = 0; i < collection.length; i++) {
         //console.log(attributeValue + " .. " + newAttributeValue );
     });
 }
+
+// Popup window
+
+const popup = document.querySelector('.popup');
+const overlay = document.querySelector('.overlay');
+const btnOpenPopup = document.querySelector('.open-popup');
+const btnClosePopup = document.querySelector('.close-popup');
+
+// Open popup
+btnOpenPopup.addEventListener('click', function(){
+    popup.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+})
+// Close popup
+btnClosePopup.addEventListener('click', function(){
+    popup.classList.add('hidden');
+    overlay.classList.add('hidden');
+})
+
+// Overlay layer
+overlay.addEventListener('click', function(){
+    popup.classList.add('hidden');
+    overlay.classList.add('hidden');
+})
+// Buttons coffee-size
+const btnOkCoffeeSize = document.querySelector('.btn-ok-coffee-size');
+const btnBigCoffeeIcon = document.querySelector('.coffee-size-big');
+const btnMediumCoffeeIcon = document.querySelector('.coffee-size-medium');
+const btnSmallCoffeeIcon = document.querySelector('.coffee-size-small');
+// 
+btnBigCoffeeIcon.addEventListener('click', function(){
+    console.log('click coffee Big');
+    btnOkCoffeeSize.classList.remove('btn-ok-coffee-size');
+    return;
+});
+// 
+btnMediumCoffeeIcon.addEventListener('click', function(){
+    console.log('click coffee Medium');
+    btnOkCoffeeSize.classList.remove('btn-ok-coffee-size');
+    return;
+    });
+    // 
+btnSmallCoffeeIcon.addEventListener('click', function(){
+    btnOkCoffeeSize.classList.remove('btn-ok-coffee-size');
+    console.log('click coffee Small');
+    return;
+    });
